886799ed122ad87f1db6e90607b823e50f4101a1
Pushpendra <86494842+XD-Deepak@users.noreply.github.com>
2021-08-22 17:06:33 +0530

    Delete presets.py

--- presets.py
+++
@@ -1,8 +0,0 @@
-class Presets(object):
-    WELCOME_TEXT = "Heya, <b>{}</b>\n<code>I am a PM Assistant Bot created by TeLe TiPs\nSend your message to my master👇🏻</code> "
-    USER_DETAILS = "<b>PM FROM:</b>\nName: {} {}\nId: {}\nUname: @{}\nScam: {}\nRestricted: {}\nStatus: {}\nDc Id: {}"
-    PM_TXT_ATT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
-    PM_MED_ATT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
-
-# Thakshaka Rathnayake - TeLe TiPs
-# ©️2021 TeLe TiPs All Rights Reserved
