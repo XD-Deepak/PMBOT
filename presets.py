cafbaf095b9fcd8b32bcc8f3ac934da5a88913ec
Pushpendra <86494842+XD-Deepak@users.noreply.github.com>
2021-08-22 17:25:36 +0530

    Create presets.py

---
+++ presets.py
@@ -0,0 +1,8 @@
+class Presets(object):
+    WELCOME_TEXT = "Heya, <b>{}</b>\n<code>I am a Personal Assistant Bot created by Pushpendra6367\nSend your message to my master👇🏻</code> "
+    USER_DETAILS = "<b>PM FROM:</b>\nName: {} {}\nId: {}\nUname: @{}\nScam: {}\nRestricted: {}\nStatus: {}\nDc Id: {}"
+    PM_TXT_ATT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
+    PM_MED_ATT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
+
+# PUSHPENDRA
+# ©️2021 Pushpendra6367 All Rights Reserved
